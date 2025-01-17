db5deb8211a5262abc0c17c47e38e08672a4f65d
DWSApi <dimycvadim37@gmail.com>
2025-01-17 21:27:39 +0200

    env

--- server/server.js
+++ server/server.js
@@ -18,6 +18,9 @@ console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PORT:", process.env.DB_PORT);
 
+console.log(process.env);
+
+
 // Создаем пул соединений
 const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
